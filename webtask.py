ca0e716e986585f05072797812b7094dc06b4238
James <59737949+jamescole404@users.noreply.github.com>
2020-02-08 10:58:02 -0500

    Create webtask.py

---
+++ webtask.py
@@ -0,0 +1,6 @@
+ <head>
+                {% if title %}
+                        <title>Flask Blog - {{ title }}</title>
+                {% else %}
+                        <title>Flask Blog</title>
+                {% endif %}
